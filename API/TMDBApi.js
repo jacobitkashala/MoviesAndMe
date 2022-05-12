e33b8fbd6dcee67e834db1d3d293b433d9d4eafc
jacobitkashala <jacobitkashala@gmail.com>
2022-05-12 09:28:58 -0400

    refactor:api token

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,6 +1,6 @@
 // API/TMDBApi.js
 
-const API_TOKEN = "API_TOKEN_HERE";
+const API_TOKEN = "c8697268acc5406f1d3c61343bbfd606";
 
 export function getFilmsFromApiWithSearchedText (text, page) {
   const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
